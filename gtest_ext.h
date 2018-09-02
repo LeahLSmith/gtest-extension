e7bac0f4bbeb51e51d4c2b38f5d66419545cf1ca
Paul Inventado <pinventado@fullerton.edu>
2018-09-02 15:24:46 -0700

    Removed inline comments that mess up the code

--- gtest_ext.h
+++ gtest_ext.h
@@ -19,18 +19,24 @@ static void catch_alarm(int sig)
 }
 
 // This macro checks whether a function executes within a given time
+//
+// setjmp is used to mark the line where longjmp from catch_alarm
+// will move to when the function call times out.
+//
+// When val is 0, the signal is initialized and the alarm timer is started. 
+// The alarm is cancelled if it finishes executing the function before the
+// alarm goes off.
+// 
+// If the alarm goes off, the handler catches the alarm and jumps to the line
+// that will then set val to 1. This results in a test failure that assigns
+// the appropriate error message.
+//
 // @param fn    function tested
 // @param usecs microseconds to wait before function is considered to have
 //              timed out
 #define ASSERT_TIME(fn, usecs) { \
-    // line where longjmp will return when alarm is signalled
     const auto val = setjmp(jmp_env); \
-    // val is 0 during initialization and when it finishes before the time limit
-    // val is 1 if alarm was caught (i.e., it took longer to execute the
-    // statements compared to the time limit).
     if (val == 0) { \
-        // assigns the alarm handler, starts the alarm, runs the function,
-        // and cances the alarm (if function finishes before time limit)
         signal(SIGALRM, catch_alarm); \
         ualarm((usecs), 0); \
         { fn; }; \
@@ -43,27 +49,25 @@ static void catch_alarm(int sig)
 // This macro indicates the start of a block that checks whether the statements
 // it contains executes with a given time. This block must be followed by an
 // ASSERT_TIMED_BLOCK_END macro
+//
+// The implementation is modeleed after ASSERT_TIME, but splits the implementation
+// into a BLOCK_START and BLOCK_END that encloses the statements that are tested
+//
 // @param name name of the timed block
 // @param usecs microseconds to wait before the statements in the block are
 //              considered to have timed out
 #define ASSERT_TIMED_BLOCK_START(name, usecs) { \
-  // line where longjmp will return when alarm is signalled
   const auto val = setjmp(jmp_env); \
-  // val is 1 if alarm was caught (i.e., it took longer to execute the
-  // statements compared to the time limit).
-  // val is 0 during initialization and when it finishes before the time limit
   if (val == 1) {\
     GTEST_FATAL_FAILURE_("      " #name " timed out (>" #usecs \
                          " microseconds)"); \
   } else { \
-      // assigns the alarm handler and starts the alarm
       signal(SIGALRM, catch_alarm); \
       ualarm((usecs), 0); \
   } }
 
-// This macro indicates the end of the block that checks whether contained
-// statements execute with a given time.
+// This macro indicates the end of the timed block that works with the
+// ASSERT_TIMED_BLOCK_START macro
 #define ASSERT_TIMED_BLOCK_END() { \
-  // cancels the alarm
    ualarm(0, 0); \
   }
