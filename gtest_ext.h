c48a5fdcaaa8082bd67c212a8edb546d12f85d49
pinventado <pinventado@fullerton.edu>
2018-09-03 13:41:12 -0700

    Add an assertion for checking the output of an exectubale file. This can be used to test programs that only have main functions.

--- gtest_ext.h
+++ gtest_ext.h
@@ -4,6 +4,40 @@
 #include <unistd.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <string>
+#include <iomanip>
+
+// Run and retrieves the output of an executable program from
+// the command line.
+//
+// @param prog_name name of the executable file
+// @param input     keyboard input sent to the program
+//
+// @return output of the program
+std::string main_output(std::string prog_name, std::string input)
+{
+  FILE *fp = popen(("echo \""+ input +"\" | ./" + prog_name).c_str(), "r");
+  char buf[1024];
+  std::string output = "";
+  while (fgets(buf, 1024, fp)) {
+    output += buf;
+  }
+  pclose(fp);
+  return output;
+}
+
+// Converts a double value to a formatted string
+//
+// @param val   value to be formatted
+// @param prec  decimal precision
+//
+// @return string representation of the double value
+std::string to_string_double(double val, const int prec = 2)
+{
+    std::ostringstream out;
+    out << std::fixed << std::setprecision(prec) << val;
+    return out.str();
+}
 
 // variable used to store jump locations used in the
 // time-based assertion macros (ASSERT_TIME, ASSERT_TIMED_BLOCK_START,
@@ -23,10 +57,10 @@ static void catch_alarm(int sig)
 // setjmp is used to mark the line where longjmp from catch_alarm
 // will move to when the function call times out.
 //
-// When val is 0, the signal is initialized and the alarm timer is started. 
+// When val is 0, the signal is initialized and the alarm timer is started.
 // The alarm is cancelled if it finishes executing the function before the
 // alarm goes off.
-// 
+//
 // If the alarm goes off, the handler catches the alarm and jumps to the line
 // that will then set val to 1. This results in a test failure that assigns
 // the appropriate error message.
@@ -35,16 +69,16 @@ static void catch_alarm(int sig)
 // @param usecs microseconds to wait before function is considered to have
 //              timed out
 #define ASSERT_TIME(fn, usecs) { \
-    const auto val = setjmp(jmp_env); \
-    if (val == 0) { \
-        signal(SIGALRM, catch_alarm); \
-        ualarm((usecs), 0); \
-        { fn; }; \
-        ualarm(0, 0); \
-    } else { \
-        GTEST_FATAL_FAILURE_("      " #fn " timed out (> " #usecs \
-        " microseconds)"); \
-    } }
+  const auto val = setjmp(jmp_env); \
+  if (val == 0) { \
+      signal(SIGALRM, catch_alarm); \
+      ualarm((usecs), 0); \
+      { fn; }; \
+      ualarm(0, 0); \
+  } else { \
+      GTEST_FATAL_FAILURE_("      " #fn " timed out (> " #usecs \
+      " microseconds)"); \
+  } }
 
 // This macro indicates the start of a block that checks whether the statements
 // it contains executes with a given time. This block must be followed by
@@ -71,3 +105,15 @@ static void catch_alarm(int sig)
 #define ASSERT_TIMED_BLOCK_END() { \
    ualarm(0, 0); \
   }
+
+// This macro checks if the output of an executable program matches an expected
+// output.
+//
+// @param prog_name name of the executable file
+// @param input     keyboard input sent to the program
+// @param output    expected output of the program
+#define ASSERT_MAIN_OUTPUT_EQ(prog_name, input, output) \
+  if ( access( prog_name, F_OK ) == -1 ) { \
+    GTEST_FATAL_FAILURE_("      cannot test '" prog_name "': no such file"); \
+  } \
+  ASSERT_EQ(main_output(prog_name, input), output);
